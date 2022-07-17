2a91e71ce28300020ad29515e958931d849480d7
Alexey <alexey.polskiy@gmail.com>
2022-07-17 14:58:43 +0500

    homework9 complient

---
+++ Task1/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 1: Задайте значения M и N. Напишите программу, которая выведет 
+// все чётные натуральные числа в промежутке от M до N с помощью рекурсии.
+// M = 1; N = 5 -> "2, 4"
+// M = 4; N = 8 -> "4, 6, 8"
+
+
+int Promt(string msg)
+{
+    System.Console.Write(msg);
+    int numberInt = int.Parse(Console.ReadLine());
+    return numberInt;
+}
+
+void PrintEvenNumbers(int firstElement, int twoElement)
+{
+    if (firstElement > twoElement) return;
+    if (twoElement % 2 == 1)
+    {
+        twoElement--;
+    }
+    PrintEvenNumbers(firstElement, twoElement - 2);
+    System.Console.Write($"{twoElement}; ");
+}
+
+int firstElement = Promt("Введите первое число отрезка M => ");
+int twoElement = Promt("Введите последнее число отрезка N => ");
+
+System.Console.Write($"На отрезке от M = {firstElement} до N = {twoElement} находятся натуральные числа ");
+PrintEvenNumbers (firstElement, twoElement);
+
+
+
+
---
+++ Task2/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 2: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N с помощью рекурсии.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8 -> 30
+
+int Promt(string msg)
+{
+    System.Console.Write(msg);
+    int numberInt = int.Parse(Console.ReadLine());
+    return numberInt;
+}
+
+int SumNumbers(int firstElement, int twoElement)
+{
+    if (firstElement > twoElement) return 0;
+    return SumNumbers(firstElement, twoElement - 1) + twoElement;
+}
+
+int firstElement = Promt("Введите первое число отрезка M => ");
+int twoElement = Promt("Введите последнее число отрезка N => ");
+
+System.Console.Write($"На отрезке от M = {firstElement} до N = {twoElement} сумма натуральных чисел равна {SumNumbers(firstElement, twoElement)}");
+;
